5bd8329b132cecd211c0656e28477be9d63c40ac
Matt Watson <mattwat@gmail.com>
2019-11-17 01:06:29 -0500

    add button link components
    open external button links in new tab

--- src/components/form/button.js
+++ src/components/form/button.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 export const Button = styled.button`
     background-color: var(--color-crimson);
@@ -7,4 +8,21 @@ export const Button = styled.button`
     color: #fff;
     padding: 0.5rem 1rem;
     text-transform: uppercase;
+`
+
+export const ButtonLink = styled(Button).attrs({ as: Link })`
+    cursor: pointer;
+    transition: filter 250ms;
+    &:hover {
+        filter: brightness(1.25);
+    }
+`
+
+export const ExternalButtonLink = styled(Button).attrs({ as: 'a', target: '_blank', rel: 'noopener noreferrer' })`
+    cursor: pointer;
+    transition: filter 250ms;
+    text-decoration: none;
+    &:hover {
+        filter: brightness(1.25);
+    }
 `
