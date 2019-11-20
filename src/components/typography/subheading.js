2f35d1ef59f4033c7a284ab4d4c757824eab72d1
Matt Watson <mattwat@gmail.com>
2019-11-20 15:10:45 -0500

    add subheading component

--- src/components/typography/subheading.js
+++ src/components/typography/subheading.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types'
 
 // 
 
-export const Title = styled.h1`
-    // border: 1px solid #f09;
-    margin: 0;
-    line-height: 4rem;
-    color: #333;
+export const Subheading = styled.h3`
+    color: #779;
+    color: ${ props => props.light ? '#dde' : '#779' };
     text-align: left;
     ${ props => props.center && 'text-align: center;' }
     ${ props => props.right && 'text-align: right;' }
 `
 
-Title.propTypes = {
+Subheading.propTypes = {
     children: PropTypes.node.isRequired,
 }
