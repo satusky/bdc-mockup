a2ed053a3a9cf21600311ad1f746e24500fc3ea4
Matt Watson <mattwat@gmail.com>
2019-11-21 13:19:14 -0500

    create data and training pages

---
+++ src/pages/data.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PageContent } from '../components/layout'
+import { Title } from '../components/typography'
+
+const DataPage = () => (
+    <PageContent width="95%" maxWidth="1080px" center gutters>
+
+        <Title>Data</Title>
+
+    </PageContent>
+)
+
+export default DataPage
---
+++ src/pages/training.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PageContent } from '../components/layout'
+import { Title } from '../components/typography'
+
+const TrainingPage = () => (
+    <PageContent width="95%" maxWidth="1080px" center gutters>
+
+        <Title>Training</Title>
+
+    </PageContent>
+)
+
+export default TrainingPage
