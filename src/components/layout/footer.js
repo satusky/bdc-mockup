cd74502eb7bd25355d121df39a665d6fc30306e8
Matt Watson <mattwat@gmail.com>
2019-11-21 09:12:31 -0500

    style footer links

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 
 export const Footer = styled.footer`
-  background-color: #fff;
-  color: #333;
-  padding: 2rem;
-  filter: drop-shadow(0 0 0.25rem rgba(0, 0, 0, 0.5));
+    background-color: #fff;
+    color: #333;
+    padding: 2rem;
+    filter: drop-shadow(0 0 0.25rem rgba(0, 0, 0, 0.5));
+    & a {
+        color: var(--color-crimson);
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
 `
