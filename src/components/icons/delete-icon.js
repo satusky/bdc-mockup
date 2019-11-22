ffa9df668a8bd72034406c248488d4d4994f7c5c
Matt Watson <mattwat@gmail.com>
2019-11-22 15:39:36 -0500

    fix merge conflict

--- src/components/icons/delete-icon.js
+++ src/components/icons/delete-icon.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 export const DeleteIcon = ({ size, ...rest }) => {
     return (
-<<<<<<< HEAD
         <svg { ...rest } version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={ `${ size }px` } height={ `${ size }px` } viewBox="0 0 24 24">
-=======
-        <svg { ...rest } version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={ `${ size }px` } height={ `${ size }px` } viewBox="0 0 24 24">
->>>>>>> eb2827d... implement basic monarch search functionality
             <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
             <path d="M0 0h24v24H0z" fill="none"/>
         </svg>
