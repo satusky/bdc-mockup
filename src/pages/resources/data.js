3fde7a4883f1084d4cc136fca8501a6f0fd7ac49
Matt Watson <mattwat@gmail.com>
2019-11-23 14:25:13 -0500

    add two sections to data page

--- src/pages/resources/data.js
+++ src/pages/resources/data.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { PageContent } from '../../components/layout'
-import { Title } from '../../components/typography'
+import { Title, Heading } from '../../components/typography'
 
 const DataPage = () => (
     <PageContent width="95%" maxWidth="1080px" center gutters>
 
         <Title>Data</Title>
+
+        <Heading>Check my Access</Heading>
+
+        <Heading>How to Get Access</Heading>
 
     </PageContent>
 )
