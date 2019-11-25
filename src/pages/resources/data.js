d4db3462387640dd930d813789a93a8f686c3de1
Matt Watson <mattwat@gmail.com>
2019-11-25 09:34:00 -0500

    add lorem ipsum paragraphs to data page

--- src/pages/resources/data.js
+++ src/pages/resources/data.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PageContent } from '../../components/layout'
-import { Title, Heading } from '../../components/typography'
+import { Title, Heading, Paragraph } from '../../components/typography'
 
 const DataPage = () => (
     <PageContent width="95%" maxWidth="1080px" center gutters>
@@ -9,7 +9,15 @@ const DataPage = () => (
 
         <Heading>Check my Access</Heading>
 
+        <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore animi nostrum odit aspernatur velit quidem expedita quam, mollitia minus officiis nisi facere minima recusandae porro atque quod veniam ipsa deleniti delectus sint molestiae modi illo. Perspiciatis explicabo labore, hic modi.
+        </Paragraph>
+
         <Heading>How to Get Access</Heading>
+
+        <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse illo ipsa odit facere accusamus hic eveniet necessitatibus earum, tempora saepe incidunt voluptatibus reiciendis soluta, eaque enim, expedita aspernatur aliquam corporis dolores ea perspiciatis amet.
+        </Paragraph>
 
     </PageContent>
 )
