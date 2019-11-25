e0e94b19f7153493ee97235813cd8a974d4192de
Matt Watson <mattwat@gmail.com>
2019-11-25 10:52:08 -0500

    remove debug logging

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ import './src/styles/customize.css'
 export const wrapPageElement = ({ element, props }) => {
     // props provide same data to Layout as Page element will get
     // including location, data, etc - you don't need to pass it
-    console.log(props)
     return (
         <DefaultLayout>
             { element }
