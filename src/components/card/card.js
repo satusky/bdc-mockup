355b79f121c46a702e4435175392b4360ad62131
Matt Watson <mattwat@gmail.com>
2019-11-26 18:04:06 -0500

    restyle new card component header and title

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { useWindowWidth } from '../../hooks'
 
 const CardContainer = styled.div`
     overflow: hidden;
@@ -11,36 +10,11 @@ const CardContainer = styled.div`
     height: 100%;
 `
 
-const CardTitle = styled.div`
-    font-size: 133%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-`
-
-const OffsetCardTitle = styled(CardTitle)`
-    position: absolute;
-    top: 2.5rem;
-    left: 50%;
-    transform: translateX(-50%);
-    border-radius: 1.5rem;
-    height: 3rem;
-    background-color: #fff;
-    color: #333;
-    padding: 0.5rem 2rem;
-    width: 75%;
-`
-
-export const Card = ({ children }) => {
-    const { isCompact } = useWindowWidth()
-    
-    return (
-        <CardContainer>
-            { children }
-        </CardContainer>
-    )
-}
+export const Card = ({ children }) => (
+    <CardContainer>
+        { children }
+    </CardContainer>
+)
 
 Card.propTypes = {
     children: PropTypes.node.isRequired,
