c30d50cd8438f002e515774664f1f3709ed98964
Matt Watson <mattwat@gmail.com>
2019-11-27 10:16:42 -0500

    remove svg style transition, leave to parent

--- src/components/icons/expand-icons.js
+++ src/components/icons/expand-icons.js
@@ -5,7 +5,6 @@ export const ExpandUpIcon = ({ size, ...rest }) => (
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
         height={ `${ size }px` } width={ `${ size }px` }
         viewBox="0 0 16 16"
-        style={{ transition: 'fill 500ms' }}
         { ...rest }
     >
         <path d="M 7.9984227,4.229289 2.3415685,9.886143 3.0486752,10.59325 7.9984227,5.643503 12.948169,10.59325 13.655276,9.886143 Z" />
@@ -16,7 +15,6 @@ export const ExpandDownIcon = ({ size, ...rest }) => (
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
         height={ `${ size }px` } width={ `${ size }px` }
         viewBox="0 0 16 16"
-        style={{ transition: 'fill 500ms' }}
         { ...rest }
     >
         <path d="M 7.9984218,10.59325 13.655276,4.936396 12.948169,4.229289 7.9984218,9.179036 3.0486755,4.229289 2.3415685,4.936396 Z" />
