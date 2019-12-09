cce70d21f8c3544a80c6d6a06b34423b2b00d80d
Matt Watson <mattwat@gmail.com>
2019-12-09 15:15:30 -0500

    buld out tools and use resource card component

--- src/pages/resources/learn.js
+++ src/pages/resources/learn.js
@@ -4,24 +4,8 @@ import { PageContent, LineBreak } from '../../components/layout'
 import { Title, Paragraph } from '../../components/typography'
 import { HexBadge } from '../../components/badge'
 import { Container as Grid, Row, Col } from 'react-grid-system'
-import { Card, CardHeader, CardBody, CardFooter } from '../../components/card'
+import { Card, CardHeader, CardBody, CardFooter, ResourceCard } from '../../components/card'
 import { List, ListItem } from '../../components/list'
-
-const SupportCard = ({ title, icon, children }) => {
-    return (
-        <Card>
-            <CardHeader bgColor="#fff" fgColor="#444" style={{ borderBottom: '1px solid var(--color-crimson' }}/>
-            <CardBody style={{ position: 'relative' }}>
-                <div style={{ position: 'absolute', top: '-3rem', left: '2rem' }}>
-                    <HexBadge>{ icon }</HexBadge>
-                </div>
-                <LineBreak count={ 2 } />
-                <h3 style={{ color: 'var(--color-crimson)' }}>{ title }</h3>
-                { children }
-            </CardBody>
-        </Card>
-    )
-}
 
 const TrainingPage = () => (
     <PageContent width="95%" maxWidth="1080px" center gutters>
@@ -46,7 +30,7 @@ const TrainingPage = () => (
                 {
                     [...Array(9).keys()].map(i => (
                         <Col key={ i } xs={ 12 } md={ 6 } lg={ 4 } style={{ marginBottom: '2rem' }}>
-                            <SupportCard title="Lorem ipsum dolor" icon={ 'ICON' }>
+                            <ResourceCard title="Lorem ipsum dolor" icon={ 'ICON' }>
                                 <List>
                                     {
                                         [...Array(Math.floor(Math.random() * 3 ) + 1).keys()].map(j => {
@@ -56,7 +40,7 @@ const TrainingPage = () => (
                                         })
                                     }
                                 </List>
-                            </SupportCard>
+                            </ResourceCard>
                         </Col>
                     ))
                 }
--- src/pages/resources/tools.js
+++ src/pages/resources/tools.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { SEO } from '../../components/seo'
 import { PageContent } from '../../components/layout'
 import { Title, Paragraph } from '../../components/typography'
+import { Container as Grid, Row, Col } from 'react-grid-system'
+import { ResourceCard } from '../../components/card'
 
 const ToolsPage = () => (
     <PageContent width="95%" maxWidth="1080px" center gutters>
@@ -16,6 +18,24 @@ const ToolsPage = () => (
         <Paragraph>
             Coming soon...
         </Paragraph>
+
+        <Grid fluid>
+            <Row>
+                {
+                    [...Array(6).keys()].map(i => {
+                        const wordCount = Math.floor(Math.random() * 35) + 5
+                        const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ab sequi obcaecati quasi doloremque nihil ea, esse non deserunt illo eligendi eius id sapiente quas aut molestiae, mollitia asperiores, inventore quaerat culpa omnis cupiditate expedita dicta natus. Facere, autem, accusamus.'.split(' ').slice(0, wordCount).join(' ')
+                        return (
+                            <Col key={ i } xs={ 12 } md={ 6 } lg={ 4 } style={{ marginBottom: '2rem' }}>
+                                <ResourceCard title="Lorem ipsum dolor" icon={ 'ICON' }>
+                                    { loremIpsum }
+                                </ResourceCard>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+        </Grid>
 
     </PageContent>
 )
